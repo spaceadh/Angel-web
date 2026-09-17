24d60fa37f6abc70c2d12d7d374379d11704f15a
spaceadh <spacesealadhesives13@gmail.com>
2026-09-17 08:41:51 +0300

    Add social media links to footer and update styles for improved layout

--- components/home/home-footer.tsx
+++ components/home/home-footer.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import logoIcon from "@/assets/malaika-logo-icon.svg";
 import styles from "./home-page.module.css";
+import { FaInstagram, FaTwitter, FaLinkedin } from "react-icons/fa6";
+
 
 export function HomeFooter() {
   return (
@@ -43,9 +45,21 @@ export function HomeFooter() {
             </Link>
           ))}
         </div>
+        {/* Add social media links */}
+        <div className={styles.footerSocial}>
+          <a href="https://www.instagram.com/malaikastudios.ke/" target="_blank" rel="noopener noreferrer">
+            <FaInstagram />
+          </a>
+          <a href="https://www.x.com/rotsilabs/" target="_blank" rel="noopener noreferrer">
+            <FaTwitter />
+          </a>
+          <a href="https://www.linkedin.com/in/alvin-victor-/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin />
+          </a>
+        </div>
         <div className={styles.footerBottom}>
           <span>© 2026 Malaika Studios. All rights reserved.</span>
-          <span>Made with the Spark ✦</span>
+          <span>Digital Presence by <a href="https://malaikastudios.rotsi.co.ke" target="_blank" rel="noopener noreferrer">Malaika Studios</a></span>
         </div>
       </div>
     </footer>
